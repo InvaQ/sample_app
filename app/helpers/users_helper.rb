71c93afedba3b829c15fac1290801151678547f7
Ivan <freerest02@gmail.com>
2016-06-03 23:22:21 +0300

    finish user edit, update, index and destroy action

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
   def gravatar_for(user, options = { size: 50 })
   	size = options[:size]
    gravatar_url = "http://media9.fast-torrent.ru/media/files/s3/mp/kh/kinopoiskru-sylvester-stallon.jpg" if user.email == "free2@mail.com"
-   gravatar_url = "http://st-fashiony.ru/pic/clothing/pic/95436/42.jpg" unless user.email == "free2@mail.com"
+   gravatar_url = "https://zhukov.github.io/webogram/img/placeholders/UserAvatar4@2x.png" unless user.email == "free2@mail.com"
    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
    # gravatar_url = "http://media9.fast-torrent.ru/media/files/s3/mp/kh/kinopoiskru-sylvester-stallon.jpg"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
---
+++ lib/tasks/sample_data.rake
@@ -0,0 +1,19 @@
+namespace :db do
+  desc "Fill database with sample data"
+  task populate: :environment do
+    User.create!(name: "Ivan",
+                 email: "free@mail.ru",
+                 password: "000000",
+                 password_confirmation: "000000",
+                 admin: true)
+    99.times do |n|
+      name  = Faker::Name.name
+      email = "example-#{n+1}@railstutorial.org"
+      password  = "password"
+      User.create!(name: name,
+                   email: email,
+                   password: password,
+                   password_confirmation: password)
+    end
+  end
+end
