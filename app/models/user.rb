38c03013c6da4f61faa200b4d88a3f0bf6725fee
InvaQ <your.freerest@gmail.com>
2016-06-15 21:27:58 +0300

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+
+	has_many :microposts, dependent: :destroy
+	
 	#before_save { self.email = email.downcase } =before_save { email.downcase! }
 	before_save { email.downcase! }
 	before_create :create_remember_token
@@ -10,12 +13,17 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, length: { minimum: 6 }
 
-	def User.new_remember_token
+  def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  def feed
+    # Это предварительное решение. См. полную реализацию в "Following users".
+    Micropost.where("user_id = ?", id)
   end
 
   private
