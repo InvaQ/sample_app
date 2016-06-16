828ded437535de26802a6210e47960792aa91725
InvaQ <your.freerest@gmail.com>
2016-06-16 17:35:50 +0300

    Add user following

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,8 +3,8 @@ module UsersHelper
 	# Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user, options = { size: 50 })
   	size = options[:size]
-   gravatar_url = "http://media9.fast-torrent.ru/media/files/s3/mp/kh/kinopoiskru-sylvester-stallon.jpg" if user.email == "free2@mail.com"
-   gravatar_url = "https://zhukov.github.io/webogram/img/placeholders/UserAvatar4@2x.png" unless user.email == "free2@mail.com"
+   #gravatar_url = "http://media9.fast-torrent.ru/media/files/s3/mp/kh/kinopoiskru-sylvester-stallon.jpg" #if user.email == "free2@mail.com"
+   gravatar_url = "https://s-media-cache-ak0.pinimg.com/favicons/b4652125911442e927685d41879cc52e7279ae1c6aaf78e614054ba3.png?25461f06861327a763a4d4f79d94f06e"#{}"https://zhukov.github.io/webogram/img/placeholders/UserAvatar4@2x.png" unless user.email == "free2@mail.com"
    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
    # gravatar_url = "http://media9.fast-torrent.ru/media/files/s3/mp/kh/kinopoiskru-sylvester-stallon.jpg"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,10 +7,10 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_content('Sign in') }
-    it { should have_title('Sign in') }
+      it { should have_content('Sign in') }
+      it { should have_title('Sign in') }
 
-    it { should_not have_link('Users') }
+      it { should_not have_link('Users') }
       it { should_not have_link('Profile') }
       it { should_not have_link('Settings') }
       it { should_not have_link('Sign out', href: signout_path) }
@@ -32,33 +32,45 @@ describe "Authentication" do
     end
   end
 
-describe "with valid information" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        sign_in user
-        #fill_in "Email",    with: user.email.upcase
-       # fill_in "Password", with: user.password
-       # click_button "Sign in"
-      end
+  describe "with valid information" do
+        let(:user) { FactoryGirl.create(:user) }
+        before do
+          sign_in user
+          #fill_in "Email",    with: user.email.upcase
+         # fill_in "Password", with: user.password
+         # click_button "Sign in"
+        end
 
-      it { should have_title(user.name) }
-      it { should have_link('Users',       href: users_path) }
-      it { should have_link('Profile',     href: user_path(user)) }
-      it { should have_link('Settings',    href: edit_user_path(user)) }
-      it { should have_link('Sign out',    href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
+        it { should have_title(user.name) }
+        it { should have_link('Users',       href: users_path) }
+        it { should have_link('Profile',     href: user_path(user)) }
+        it { should have_link('Settings',    href: edit_user_path(user)) }
+        it { should have_link('Sign out',    href: signout_path) }
+        it { should_not have_link('Sign in', href: signin_path) }
 
-      describe "followed by signout" do
-        before { click_link "Sign out" }
-        it { should have_link('Sign in') }
+        describe "followed by signout" do
+          before { click_link "Sign out" }
+          it { should have_link('Sign in') }
+        end
       end
-    end
-end
+  end
 
 describe "authorization" do
 
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
+      
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
 
       describe "in the Microposts controller" do
 
@@ -116,7 +128,15 @@ describe "authorization" do
           before { visit users_path }
           it { should have_title('Sign in') }
         end
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
 
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
       end
 
     end
@@ -131,15 +151,15 @@ describe "authorization" do
 				        before { delete user_path(user) }
 				        specify { expect(response).to redirect_to(root_url) }
 				      end
-				    end
+				end
 
 
 
 
     		describe "as wrong user" do
-     		 let(:user) { FactoryGirl.create(:user) }
-    		  let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-     		 before { sign_in user, no_capybara: true }
+         		let(:user) { FactoryGirl.create(:user) }
+        		let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+         		before { sign_in user, no_capybara: true }
 
 			      describe "submitting a GET request to the Users#edit action" do
 			        before { get edit_user_path(wrong_user) }
@@ -151,20 +171,20 @@ describe "authorization" do
 			        before { patch user_path(wrong_user) }
 			        specify { expect(response).to redirect_to(root_url) }
 			      end
-			end
+        end
 
 
 
-      describe "as admin user" do
-    let(:admin) { FactoryGirl.create(:admin) }
-    before { sign_in admin, no_capybara: true }
+        describe "as admin user" do
+          let(:admin) { FactoryGirl.create(:admin) }
+          before { sign_in admin, no_capybara: true }
 
-    describe "should not be able to delete themselves via #destroy action" do
-      specify do
-        expect { delete user_path(admin) }.not_to change(User, :count).by(-1)
-      end
-    end
-  end
+          describe "should not be able to delete themselves via #destroy action" do
+            specify do
+              expect { delete user_path(admin) }.not_to change(User, :count).by(-1)
+            end
+          end
+        end
 
   
 end
